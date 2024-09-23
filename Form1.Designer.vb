a7d0cdc787adc704471e2d731d60f3c107a0c9a5
hp <hp@DESKTOP-6O7K1F7>
2024-09-23 07:46:29 +0530

    chnges confirm

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,17 +22,41 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Location = New System.Drawing.Point(12, 145)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(1058, 377)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(411, 40)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(166, 61)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1082, 534)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListView1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents Button1 As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,6 +1,19 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        MessageBox.Show("Vaibhav")
-        MessageBox.Show("Rohit Khatri")
+        ListView1.Columns.Add("P_ID", 90, HorizontalAlignment.Center)
+        ListView1.Columns.Add("P_NAME", 180, HorizontalAlignment.Center)
+        ListView1.Columns.Add("BRAND", 90, HorizontalAlignment.Center)
+        ListView1.Columns.Add("C_ID", 80, HorizontalAlignment.Center)
+        ListView1.Columns.Add("PUR_PRICE", 90, HorizontalAlignment.Center)
+        ListView1.Columns.Add("MRP", 90, HorizontalAlignment.Center)
+        ListView1.Columns.Add("STK_QNT", 180, HorizontalAlignment.Center)
+        ListView1.Columns.Add("HSN", 110, HorizontalAlignment.Center)
+        ListView1.Columns.Add("DIS", 100, HorizontalAlignment.Center)
+        ListView1.Columns.Add("IVT_DATE", 150, HorizontalAlignment.Center)
+        Button1.Text = "NEW"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
     End Sub
 End Class
